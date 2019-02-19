2dc3eb9d03b352e67a1a5bb49b312a2f3b3bbfb8
Agustin Oria <agustin.oria@gmail.com>
2019-02-19 20:28:36 -0300

    Avances en el controlador.

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
 
 require_once($_SERVER['DOCUMENT_ROOT']. '/view.php');
+require_once($_SERVER['DOCUMENT_ROOT']. '/minprod_validator.php');
 
 $view = new view();
 
-switch($_REQUEST['ajax']){
+switch(true){
+	case $_POST['login']:
+		$minprod_validator = new minprod_validator();
+		print $minprod_validator->validate_login($_POST['login']);
+		break;
 	default:
 		print $view->get_initial_html();
 }
