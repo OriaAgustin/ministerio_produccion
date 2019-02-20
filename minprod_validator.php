1e87e941a3668aa6b149d41eafc26240c2d45995
Agustin Oria <agustin.oria@gmail.com>
2019-02-20 00:45:27 -0300

    Validacion basica.

--- minprod_validator.php
+++ minprod_validator.php
@@ -1,17 +1,45 @@
 <?php
+
+require_once($_SERVER['DOCUMENT_ROOT']. '/kint.phar');
+
 class minprod_validator{
     private $mysql;
+    private $tables = array();
+    private $idfields = array();
 
     public function __construct(){
     	$this->set_mysql();
+    	$this->set_tables();
+    	$this->set_id_fields();
     }
 
     private function set_mysql(){
-    	$this->mysql = new mysqli("localhost", "root");
+    	//No deberia usarse el usuario root y menos sin contrasena.
+    	$this->mysql = new mysqli('localhost', 'root', '', 'ministerio_produccion');
     }
 
+    private function set_tables(){
+    	$this->tables = array('usuario_interno' => 'usuarios_internos', 'empresa' => 'empresas');
+    }
+
+    private function set_id_fields(){
+    	$this->id_fields = array('usuario_interno' => 'email', 'empresa' => 'cuit');
+    }
+    /*
+		Meter lo de la validacion del cuit y lo de la validacion del mail?
+		ESCAPAR ARGUMENTOS
+    */
     public function validate_login($login){
-    	return "MySQL Server info: ". $this->mysql->host_info. " cuit:". $login['id_login']. " password:". $login['password_login'];
-    }
+    	$query =	'SELECT * FROM '. $this->tables[$login['type_login']]. 
+    				' WHERE '. $this->id_fields[$login['type_login']]. '="'. $login['id_login']. 
+    				'" AND password=PASSWORD("'. $login['password_login']. '")';
+    	$result = $this->mysql->query($query);
+		$user = $result->fetch_assoc();
+    	
+    	if($user){
+			return true;
+		}
 
+    	return false;
+    }
 }
