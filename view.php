28d5780d3fe853982932b925febd584b50bebcaa
Agustin Oria <agustin.oria@gmail.com>
2019-02-20 01:25:01 -0300

    Se genero la vista de error de logueo y se avanzo sobre la vista principal.

--- view.php
+++ view.php
@@ -1,6 +1,9 @@
 <?php
+/*Separar en clases hijas y padre?*/
 class view{
     private $initial_html;
+    private $main_view;
+    private $login_error;
 
     public function __construct(){
     	$this->set_initial_html();
@@ -8,9 +11,11 @@ class view{
 
     private function set_initial_html(){
         $head = $this->generate_head();
-        $body = $this->generate_body();
 
-        $html = '<!DOCTYPE html><html lang="es">'. $head. $body. '</html>';
+        $login_div = $this->generate_login_div();
+        $body = $this->generate_body($login_div);
+
+        $html = $this->generate_html($head. $body);
 
         $this->initial_html = $html;
     }
@@ -25,41 +30,70 @@ class view{
         return $head;
     }
 
-    private function generate_body(){
-
-    	$login_div = $this->generate_login_div();
-
-        $body = '<body>'. $login_div. '</body>';
+    private function generate_body($inner_html){
+        $body = '<body>'. $inner_html. '</body>';
 
         return $body;
     }
 
-   	private function generate_login_div(){
-   		$login_select =	'<select id="login_select" onchange="change_profile_type(event);">
-    						<option value="empresa">Empresa</option>
-    						<option value="usuario_interno">Usuario interno</option>	
-    					</select>';
-    	$login_inputs = '<input id="id_login_input" class="login_input" type="text" placeholder="Cuit" name="login[id_login]">
-   						<input id="password_login_input" class="login_input" type="password" placeholder="Contraseña" name="login[password_login]">';
-    	//$login_data_div = '<div id="login_data_div">'. $login_inputs. '</div>';
+    private function generate_html($inner_html){
+    	$html = '<!DOCTYPE html><html lang="es">'. $inner_html. '</html>';
 
+    	return $html;
+    }
+
+   	private function generate_login_div(){
+   		$login_select =	'<select id="login_select" onchange="change_profile_type(event);" name="login[type_login]">
+    						<option value="usuario_interno">Usuario interno</option>	
+    						<option value="empresa">Empresa</option>
+    					</select>';
+    	$login_inputs = '<input id="id_login_input" class="login_input" type="text" placeholder="Email" name="login[id_login]">
+   						<input id="password_login_input" class="login_input" type="password" placeholder="Contraseña" name="login[password_login]">';
     	$login_button = '<input type="submit" value="Ingresar">';
 
-        $login_form = '<form id="login_form" action="index.php" method="post">'. $login_select. $login_inputs. $login_button. '</form>';
+        $login_form = '<div class="login_wrapper"><form id="login_form" action="index.php" method="post">'. $login_select. $login_inputs. $login_button. '</form></div>';
 
    		return $login_form;
    	}
 
-    public function get_initial_html(){
+   	public function get_login(){
         return $this->initial_html;
     }
 
-    public function validate_login($post){
-    	foreach($post as $key => $value){
-    		echo $key;
-    		echo '<br>';
-    		echo $value;
-    		echo '<br>';
-    	}
+    public function get_main_view(){
+    	$this->set_main_view();
+
+    	return $this->main_view;
+    }
+
+    private function set_main_view(){
+    	$head = $this->generate_head();
+
+    	$main_view = '<p>Prueba</p>';
+
+    	$body = $this->generate_body($error_form);
+
+        $html = $this->generate_html($head. $body);
+
+    	$this->login_error = $html;
+    }
+
+    public function get_login_error(){
+    	$this->set_login_error();
+    	
+    	return $this->login_error;
+    }
+
+    private function set_login_error(){
+        $head = $this->generate_head();
+
+    	$message =	'<p>Usuario o contrase&nacute;a no validos.</p>';
+    	$retry_button = '<input type="submit" value="Volver a intentar">';
+        $error_form = '<div class="login_wrapper"><form id="error_form" action="index.php" method="post">'. $message. $retry_button. '</form></div>';
+        $body = $this->generate_body($error_form);
+
+        $html = $this->generate_html($head. $body);
+
+    	$this->login_error = $html;
     }
 }
